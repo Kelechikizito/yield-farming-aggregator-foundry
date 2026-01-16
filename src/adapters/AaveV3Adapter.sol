c4e8994824e77b223c6ae921b6e6c7a9b44b24fc
kelechikizito <kaylaychi77@gmail.com>
2026-01-16 07:15:37 +0100

    update: aave-adapter

--- src/adapters/AaveV3Adapter.sol
+++ src/adapters/AaveV3Adapter.sol
@@ -6,6 +6,8 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 /// @dev This interface contract is the interface for the main user-facing contract for aave protocol - This is like Comet for Compound
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {DataTypes} from "@aave/core-v3/contracts/protocol/libraries/types/DataTypes.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 
 /**
  * @title AaveV3Adapter
@@ -14,29 +16,42 @@ import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
  * @dev
  */
 // note: The aave pool contract is the main entry point into the AAVE protocol
-abstract contract AaveV3Adapter is IProtocolAdapter {
+contract AaveV3Adapter is IProtocolAdapter {
+    /*//////////////////////////////////////////////////////////////
+                              ERRORS
+    //////////////////////////////////////////////////////////////*/
+
     error AaveV3Adapter__InvalidAavePoolAddress();
+
+    /*//////////////////////////////////////////////////////////////
+                            TYPE DECLARATIONS
+    //////////////////////////////////////////////////////////////*/
 
     using SafeERC20 for IERC20;
 
-    /// @dev The interface of the Aave V3 pool
-    IPool private immutable i_aavePool;
+    /// @dev The interface of the Aave V3 poolAddressProvider contract
+    IPoolAddressesProvider private immutable i_aavePoolAddressesProvider;
+
+    /*/////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    /////////////////////////////////////////////////////////*/
 
     constructor(
-        address _aavePool /*,address _yieldAggregator*/
+        address _aavePoolAddressesProvider /*,address _yieldAggregator*/
     ) {
-        if (_aavePool == address(0)) revert AaveV3Adapter__InvalidAavePoolAddress();
-        i_aavePool = IPool(_aavePool);
+        if (_aavePoolAddressesProvider == address(0)) revert AaveV3Adapter__InvalidAavePoolAddress();
+        i_aavePoolAddressesProvider = IPool(_aavePoolAddressesProvider);
     }
 
-    function deposit(uint256 amount, address token) external returns (uint256 shares) {
-        //         function supply(
-        //     address asset,
-        //     uint256 amount,
-        //     address onBehalfOf,
-        //     uint16 referralCode
-        // ) public virtual override {}
+    /*//////////////////////////////////////////////////////////////
+                        EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(address token, uint256 amount) external returns (uint256 shares) {
+        i_aavePool.supply(token, amount, address(this), 0);
     }
 
-    function withdraw(address token, uint256 shares) external returns (uint256 amount) {}
+    function withdraw(address token, uint256 shares) external returns (uint256 amount) {
+        i_aavePool.withdraw(token, amount, address(this));
+    }
 }
