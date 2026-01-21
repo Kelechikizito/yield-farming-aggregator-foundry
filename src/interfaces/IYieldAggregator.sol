0ea75d8c31b830e43d11fd5076bad2d453b3f025
kelechikizito <kaylaychi77@gmail.com>
2026-01-21 12:05:05 +0100

    update: yield aggregator interface

---
+++ src/interfaces/IYieldAggregator.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+interface IYieldPeer {
+    struct Strategy {
+        uint64 chainSelector;
+        Protocol protocol;
+    }
+
+    enum Protocol {
+        Aave,
+        Compound
+    }
+
+    struct DepositData {
+        address depositor; // user who deposited USDC and will receive shares
+        uint256 amount; // amount of USDC deposited
+        uint256 totalValue; // total value of the system (this is updated on the strategy chain)
+        uint256 shareMintAmount; // amount of shares minted to the depositor (this is updated on the parent chain callback)
+        uint64 chainSelector; // chain selector of the chain the deposit originated from
+    }
+
+    struct WithdrawData {
+        address withdrawer; // user who is withdrawing USDC
+        uint256 shareBurnAmount; // amount of shares burned
+        uint256 totalShares; // total shares in the system (updated on the parent chain)
+        uint256 usdcWithdrawAmount; // amount of USDC to withdraw
+        uint64 chainSelector; // chain selector of the chain the withdrawal originated from
+    }
+
+    function deposit(uint256 amountToDeposit) external;
+    function getStrategyPool() external view returns (address);
+    function getTotalValue() external view returns (uint256);
+    function getCompound() external view returns (address);
+    function getAave() external view returns (address);
+    function setCCIPGasLimit(uint256 gasLimit) external;
+    function setAllowedChain(uint64 chainSelector, bool allowed) external;
+    function setAllowedPeer(uint64 chainSelector, address peer) external;
+    function getAllowedChain(uint64 chainSelector) external view returns (bool);
+    function getAllowedPeer(uint64 chainSelector) external view returns (address);
+}
